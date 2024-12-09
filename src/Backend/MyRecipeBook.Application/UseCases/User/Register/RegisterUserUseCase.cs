ba21d35d1403ebd5044842d6b4a7b19a335e2e44
Giovanna Gamba <giovannagamba9@gmail.com>
2024-12-09 13:15:25 -0300

    Create Exceptions

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,35 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+        {
+
+            Validate(request);
+
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name,
+            };
+        }
+
+        private void Validate(RequestRegisterUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+
+            var result = validator.Validate(request);
+
+            if (result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errorMessages);
+            }
+
+        }
+    }
+}
