14b4cd71054f8319738862c62cae6431d83dc3ae
Giovanna Gamba <giovannagamba9@gmail.com>
2024-12-20 08:49:47 -0300

    Configure in-memory database

--- src/Backend/MyRecipeBook.Infraestructure/Extensions/ConfigurationExtension.cs
+++ src/Backend/MyRecipeBook.Infraestructure/Extensions/ConfigurationExtension.cs
@@ -4,6 +4,10 @@ namespace MyRecipeBook.Infraestructure.Extensions
 {
     public static class ConfigurationExtension
     {
+        public static bool IsUnitTestEnviroment(this IConfiguration configuration)
+        {
+            configuration.Get<bool>("InMemoryTest");
+        }
         public static string ConnectionString(this IConfiguration configuration)
         {
             var databaseType = configuration.GetConnectionString("Connection");
